b0a5663685ce42e681e4e110f90523311fdfa11e
Master <61457laize@gmail.com>
2023-06-30 19:00:38 +0700

    added app .gitignore

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class App {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Welcome to the Brain Games!");
+
+    }
+}
